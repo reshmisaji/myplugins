5aa1dcdd4466253305bbb7adddbeefc27da1708c
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-24 10:43:15 +0530

    add some browser shortcuts

--- aliases.zsh
+++ aliases.zsh
@@ -5,7 +5,10 @@ alias commit="git commit"
 alias n="node"
 alias push="git push"
 alias c="clear;ls" 
-alias mcd=". ~/projects/shell_scripts/createDirectory.sh"
-alias .="~"
 alias v="vim"
-
+alias typing="open https://www.typingclub.com/sportal/program-3.game"
+function openGoogle(){
+  open "https://www.google.com/"
+}
+zle -N openGoogle
+bindkey "^[g" openGoogle
