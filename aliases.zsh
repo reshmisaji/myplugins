cf9c6b7955b5d13390b2645215969be0bfaab205
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-24 14:07:47 +0530

    add another plugin which contains shortcuts for going to browser

--- aliases.zsh
+++ aliases.zsh
@@ -6,9 +6,3 @@ alias n="node"
 alias push="git push"
 alias c="clear;ls" 
 alias v="vim"
-alias typing="open https://www.typingclub.com/sportal/program-3.game"
-function openGoogle(){
-  open "https://www.google.com/"
-}
-zle -N openGoogle
-bindkey "^[g" openGoogle
---
+++ browsershortcuts.zsh
@@ -0,0 +1,10 @@
+alias typing="open https://www.typingclub.com/sportal/program-3.game"
+
+function openGoogle(){
+  open "https://www.google.com/"
+}
+
+zle -N openGoogle
+bindkey "^g" openGoogle
+
+alias okta="open https://thoughtworks.okta.com/"
