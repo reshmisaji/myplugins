d35a23355b86a9e8f755978061ce1121502c7720
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-24 18:19:11 +0530

    add shortcuts for going to people's github page

---
+++ zsh-plugins/githubShortcuts.zsh
@@ -0,0 +1,5 @@
+alias mygit="open https://github.com/reshmisaji"
+alias keerthy="open https://github.com/keerthyeb"
+alias swapnil="open https://github.com/swapnillothe"
+alias rahul="open https://github.com/rahulmadaan"
+
