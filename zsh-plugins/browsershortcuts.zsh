bf1e1ce749e7348423df01948aa95e2cb308b1ec
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-25 20:55:46 +0530

    add some more shortcuts for zsh

--- zsh-plugins/browsershortcuts.zsh
+++ zsh-plugins/browsershortcuts.zsh
@@ -1,10 +1,8 @@
-alias typing="open https://www.typingclub.com/sportal/program-3.game"
-
 function openGoogle(){
   open "https://www.google.com/"
 }
-
 zle -N openGoogle
-bindkey "^g" openGoogle
 
+bindkey "^g" openGoogle
 alias okta="open https://thoughtworks.okta.com/"
+alias typing="open https://www.typingclub.com/sportal/program-3.game"
